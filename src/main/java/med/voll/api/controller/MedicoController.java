461eda91b520529bdfe32cb8903d1cfcc182fc10
João Vicente <joaovicenterangel@gmail.com>
2023-03-06 19:37:54 -0300

    Adding GET requests

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,14 +1,17 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -22,13 +25,14 @@ public class MedicoController {
     private MedicoRepository repository;
 
     /*
-    Nessa classe está sendo utilizado o padrão DTO - Data Transfer Object, um padrão muito utilizado em API's para
+    Nessa classe é utilizado o padrão DTO — Data Transfer Object, um padrão muito utilizado em API's para
     representar os dados que chegam e são devolvidos à API. Sempre que for necessário receber ou devolver uma informação
     da API, esse padrão será utilizado.
      */
     @PostMapping
+    // Anotação 'Transactional' é necessária, pois o método realiza alterações no banco de dados
     @Transactional
-    public void cadastrar(@RequestBody DadosCadastroMedico dados) {
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         /*
         A anotação 'RequestBody' mapeia que o parâmetro json vem do corpo da requisição, enviada pelo postman.
          */
@@ -37,6 +41,17 @@ public class MedicoController {
         O método save é herdado pelo repository e ele é o responsável por salvar os dados no banco de dados.
          */
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedico> listar(Pageable paginacao) {
+        /* O método findAll retorna um objeto do tipo Medico, mas é necessário que retorne um objeto do tipo
+        DadosListagemMedico, por isso há a conversão entre o tipo de retorno.
+
+            O 'Pageable' realiza a paginação automaticamente.
+         */
+
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 
 }
--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,5 +1,10 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 
 /*
@@ -7,5 +12,23 @@ Essa é uma classe do tipo Record. O java criará uma classe imutável, quando n
 um atributo, os métodos get, set e construtores serão gerados sem a necessidade de digitar tudo manualmente.
 Dessa maneira o código fica muito mais enxuto e menos verboso.
  */
-public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+public record DadosCadastroMedico(
+        //'NotBlank' verifica se está vazio e se não é nulo, mas apenas para campos do tipo string
+        @NotBlank
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        String telefone,
+        @NotBlank
+        // 'Pattern' é uma expressão regular que identifica o CRM como tendo de 4 a 6 dígitos
+        @Pattern(regexp = "\\d{4,6}")
+        String crm,
+        @NotNull
+        Especialidade especialidade,
+        @NotNull
+        // 'Valid' serve para validar quando há outro objeto do tipo record, que também terá validações internas
+        @Valid
+        DadosEndereco endereco) {
 }
